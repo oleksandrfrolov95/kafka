477c28fc32f126a5db75b6b6a1ff5b8e4ecd7a18
Oleksandr Frolov <oleksandr.frolov@ericsson.com>
2022-05-25 19:06:25 +0300

    Add code

---
+++ kafka-consumer/src/main/java/com/dante/kafkaconsumer/ConsumerService.java
@@ -0,0 +1,14 @@
+package com.dante.kafkaconsumer;
+
+import com.dante.kafkaconsumer.Message;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ConsumerService {
+
+    @KafkaListener(topics = "messages", groupId = "message_group_id")
+    public void consume(Message message){
+        System.out.println("Consuming the message: " + message);
+    }
+}
